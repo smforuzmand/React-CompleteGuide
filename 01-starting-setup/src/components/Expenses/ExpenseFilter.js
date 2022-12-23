42b88d51b220561af0a914becf9c3dde9e21044f
smforuzmand <career.forouzmand@gmail.com>
2022-12-23 22:18:27 +0100

    state up, expenseFilter added

--- 01-starting-setup/src/components/Expenses/ExpenseFilter.js
+++ 01-starting-setup/src/components/Expenses/ExpenseFilter.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseFilter.css'
-const ExpenseFilter = () => {
+const ExpenseFilter = (props) => {
 
-    const changeEventHandler = () => {
+    const [dateChange, setDateChange] = useState(props.dateChange)
+
+    const changeEventHandler = (event) => {
+
+        setDateChange(event.target.value);
+
 
     }
     return (
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select onChange={ }>
+                <select onChange={changeEventHandler}>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
