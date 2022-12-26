6cfd56939610ab465b28afa78f78d6d38533d54b
smforuzmand <career.forouzmand@gmail.com>
2022-12-26 10:11:39 +0100

    card wrapped inside of a Li in favour of semantic

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -15,14 +15,16 @@ const ExpenseItem = (props) => {
         setTitle("updated!!!!!")
     }
     return (
-        <Card className='expense-item'>
-            <ExpenseDate date={props.date} />
-            <div className='expense-item__description'>
-                <h2>{title}</h2>
-                <div className='expense-item__price'>${props.amount}</div>
-            </div>
-            <button onClick={eventHandler}>click me</button>
-        </Card>
+        <li>
+            <Card className='expense-item'>
+                <ExpenseDate date={props.date} />
+                <div className='expense-item__description'>
+                    <h2>{title}</h2>
+                    <div className='expense-item__price'>${props.amount}</div>
+                </div>
+                <button onClick={eventHandler}>click me</button>
+            </Card>
+        </li>
     );
 }
 
