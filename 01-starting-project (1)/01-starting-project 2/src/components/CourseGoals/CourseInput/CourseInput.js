c627348770e3f667fe2e7462fc1b771118f49414
smforuzmand <career.forouzmand@gmail.com>
2022-12-31 09:55:38 +0100

    adding the colore validation

--- 01-starting-project (1)/01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project (1)/01-starting-project 2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -6,20 +6,29 @@ import './CourseInput.css';
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
 
+  const [isValid, setIsVAlid] = useState(true);
+
+
+
+
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setIsVAlid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
+        <input style={{ borderColor }} type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
